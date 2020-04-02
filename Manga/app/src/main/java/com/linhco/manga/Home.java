1b02acaedf9bf04440c2672c6c7a2deb9f80a033
Linh Vu <62274969+LinhVu2303@users.noreply.github.com>
2020-04-02 17:02:25 +0700

    AppManga

---
+++ Manga/app/src/main/java/com/linhco/manga/Home.java
@@ -0,0 +1,33 @@
+package com.linhco.manga;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("ĐĂNG KÝ"); // đổi tên thanh bar
+        String title = actionBar.getTitle().toString();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Manga/app/src/main/java/com/linhco/manga/MainActivity.java
@@ -0,0 +1,39 @@
+package com.linhco.manga;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSignUp, btnLogin;
+    TextView tvForgotPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignUp = findViewById(R.id.btn_signup);
+        btnLogin = findViewById(R.id.btn_login);
+        tvForgotPass = findViewById(R.id.tv_forgot);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
