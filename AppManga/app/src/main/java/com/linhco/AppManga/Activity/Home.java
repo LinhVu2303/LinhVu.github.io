b5728ed987810f96565552c6aa5eaa2181dbf430
Linh Vu <62274969+LinhVu2303@users.noreply.github.com>
2020-04-08 11:24:28 +0700

    Manga

--- AppManga/app/src/main/java/com/linhco/AppManga/Activity/Home.java
+++ AppManga/app/src/main/java/com/linhco/AppManga/Activity/Home.java
@@ -1,6 +1,5 @@
 package com.linhco.AppManga.Activity;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,8 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
+
 
 import com.linhco.AppManga.Adapter.HomeAdapter;
 import com.linhco.AppManga.Object.Manga;
@@ -20,7 +18,7 @@ import com.linhco.AppManga.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity  {
     private RecyclerView manga;
     private HomeAdapter adapter;
     private List<Manga> listManga;
@@ -33,19 +31,17 @@ public class Home extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("DANH SÁCH TRUYỆN"); // đổi tên thanh bar
 
-
         initView();
     }
 
     private void initView() {
         listManga = new ArrayList<>();
-        for(int i = 0; i < 5; i++){
+
             listManga.add(new Manga("Dragon ball", "Một cậu bé có đuôi khỉ tên là Goku được tìm thấy bởi một ông lão sống một mình trong rừng",R.drawable.aa));
             listManga.add(new Manga("Dragon Ball Super", "Câu chuyện của Dragon Ball Super diễn ra ngay sau khi chiến đấu với Ma Nhân Bư",R.drawable.bb));
             listManga.add(new Manga("Naruto","Naruto là một cậu bé có mơ ước trở thành hokage của làng Konoha",R.drawable.cc));
             listManga.add(new Manga("one piece", "One Piece xoay quanh 1 nhóm cướp biển được gọi là Băng Hải tặc Mũ Rơm - Straw Hat Pirates ", R.drawable.dd));
-            listManga.add(new Manga("nu hong ai cap", "",R.drawable.ee));
-        }
+            listManga.add(new Manga("nu hoang ai cap", "",R.drawable.ee));
 
         manga = findViewById(R.id.rcv_list_manga); //anh xa
         manga.setHasFixedSize(true);
--- AppManga/app/src/main/java/com/linhco/AppManga/Activity/Info.java
+++ AppManga/app/src/main/java/com/linhco/AppManga/Activity/Info.java
@@ -4,12 +4,26 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.linhco.AppManga.R;
 
-public class Info extends AppCompatActivity {
+import java.util.prefs.PreferenceChangeEvent;
+
+public class Info extends AppCompatActivity implements View.OnClickListener{
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Button btnSaveInfo;
+    EditText nameInfo, phoneInfo, emailInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,28 @@ public class Info extends AppCompatActivity {
         actionBar.setTitle("THÔNG TIN TÀI KHOẢN"); // đổi tên thanh bar
         String title = actionBar.getTitle().toString();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        initPreference();
+
+        nameInfo = findViewById(R.id.edt_nameInfo);
+        phoneInfo = findViewById(R.id.edt_phoneInfo);
+        emailInfo = findViewById(R.id.edt_emailInfo);
+        btnSaveInfo = findViewById(R.id.btn_saveInfo);
+
+        String saveName = sharedPreferences.getString("NAME", "");
+        String savePhone = sharedPreferences.getString("PHONE", "");
+        String saveEmail = sharedPreferences.getString("EMAIL", "");
+
+        nameInfo.setText(saveName);
+        phoneInfo.setText(savePhone);
+        emailInfo.setText(saveEmail);
+
+        btnSaveInfo.setOnClickListener(this);
+    }
+
+    private void initPreference(){
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPreferences.edit();
     }
 
     @Override
@@ -33,5 +69,24 @@ public class Info extends AppCompatActivity {
             default:break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnSaveInfo){
+            // Nếu click vào nút Save, ta sẽ lưu dữ liệu lại.
+            String name = nameInfo.getText().toString();
+            editor.putString("NAME", name);
+
+            String phone = phoneInfo.getText().toString();
+            editor.putString("PHONE", phone);
+
+            String email = emailInfo.getText().toString();
+            editor.putString("EMAIL", email);
+
+            editor.commit();
+
+            Toast.makeText(Info.this, "Đã lưu thông tin", Toast.LENGTH_SHORT).show();
+        }
     }
 }
